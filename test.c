926f0b444d6f89caa8513ea306ed06861b782fdc
gallit0 <mateogesede@gmail.com>
2022-07-08 07:12:32 -0700

    1

--- test.c
+++ test.c
@@ -3,14 +3,14 @@
 int main(void)
 {
 	unsigned int n = 2147484671;
-	int n2 = 26;
+	int n2 = 526;
 	printf("%u\n", n);
 	_printf("%u\n", n);
 	
 	printf("%o\n", n2);
 	_printf("%o\n", n2);
 
-
-
+	printf("%x\n", n2);
+	_printf("%x\n", n2);
 	return 0;
 }
