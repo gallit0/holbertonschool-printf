17520b71457dedca2c9e61dd1c8712380ac99df7
gallit0 <mateogesede@gmail.com>
2022-07-08 06:37:29 -0700

    1

--- printf.c
+++ printf.c
@@ -99,6 +99,48 @@ int p_bin(va_list ptr)
 	return (p_binary(n));
 }
 /**
+ * p_un_int - print an unsigned integer
+ * @ptr: variadic list
+ * Return: counter
+ */
+int p_un_int(va_list ptr)
+{
+	unsigned long int n = va_arg(ptr, unsigned int);
+	unsigned long int i = 1;
+	int counter = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	for (; i <= n; i *= 10)
+		counter++;
+	i /= 10;
+
+	for (; i > 0; i /= 10)
+		_putchar(((n / i) % 10) + '0');
+	return (counter);
+}
+/**
+ * p_oct - print an integer as an octal
+ * @ptr: variadic list
+ */
+int p_octal(unsigned int n)
+{
+	int counter = 0;
+	if (n <= 7)
+		return (_putchar(n + '0'));
+	counter = p_binary(n / 8);
+	return (_putchar('0' + n % 8) + counter);
+}
+int p_oct(va_list ptr)
+{
+	unsigned int n = va_arg(ptr, unsigned int);
+
+	return (p_octal(n));
+}
+/**
  * check_printf - checks printf
  * @format: string
  * Return: -1 if mistake 1 if ok
@@ -130,10 +172,12 @@ int get_function(const char formi, va_list ptr)
 		{"d", p_int},
 		{"i", p_int},
 		{"b", p_bin},
+		{"u", p_un_int},
+		{"o", p_oct},
 	};
 	int counter = 0, i;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < 6; i++)
 	{
 		if (*dat[i].type == formi)
 			return (counter += dat[i].f(ptr));
---
+++ test.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+	unsigned int n = 2147484671;
+	int n2 = 26;
+	printf("%u\n", n);
+	_printf("%u\n", n);
+	
+	printf("%o\n", n2);
+	_printf("%o\n", n2;
+
+
+
+	return 0;
+}
