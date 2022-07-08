c6e345789d336802a21e3d709306bcf57c1ce5ad
gallit0 <mateogesede@gmail.com>
2022-07-08 10:13:16 -0700

    1

--- printf.c
+++ printf.c
@@ -183,7 +183,16 @@ int p_STRING(va_list ptr)
 	int counter = 0;
 	char *str = va_arg(ptr, char *);
 	int i;
-
+	if (!str)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		counter = 6;
+	}
 	for(i = 0; str[i]; i++)
 	{
 		if (str[i] < 32 || str[i] >= 127)
@@ -197,6 +206,7 @@ int p_STRING(va_list ptr)
 			_putchar(str[i]);
 		}
 	}
+	return (counter);
 }
 /**
  * check_printf - checks printf
